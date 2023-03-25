87a4a878af7cfd2e93a3139a1a975174a21fdf23
Arunava-Barua <rahulbarua31@gmail.com>
2023-03-25 12:09:45 +0530

    Test cases for multi-sig and crowdfunding basic smart contract

--- Assignments/Week 2/Pod 4/Arunava/multiSig.py
+++ Assignments/Week 2/Pod 4/Arunava/multiSig.py
@@ -47,3 +47,54 @@ class MultiSigWallet(sp.Contract):
         # reset the approvals for all transactions
         sp.verify(sp.sender in self.data.owners, message="sender must be an owner")
         self.data.approvals = {}
+
+    def test():
+        # create 3 test accounts
+        owner1 = sp.test_account("Owner 1")
+        owner2 = sp.test_account("Owner 2")
+        owner3 = sp.test_account("Owner 3")
+    
+        # deploy contract with 2 out of 3 owners required for approval
+        multi_sig_wallet = MultiSigWallet([owner1.address, owner2.address, owner3.address], 2)
+        scenario = sp.test_scenario()
+        scenario += multi_sig_wallet
+    
+        # deposit funds into the contract
+        scenario += multi_sig_wallet.deposit().run(sender=owner1, amount=sp.mutez(100))
+        scenario += multi_sig_wallet.deposit().run(sender=owner2, amount=sp.mutez(50))
+        scenario += multi_sig_wallet.deposit().run(sender=owner3, amount=sp.mutez(25))
+    
+        # withdraw funds from the contract
+        scenario += multi_sig_wallet.withdraw(sp.mutez(25)).run(sender=owner1)
+    
+        # transfer funds to another address
+        recipient = sp.test_account("Recipient").address
+        scenario += multi_sig_wallet.transfer(recipient, sp.mutez(50)).run(sender=owner2)
+    
+        # create transaction that requires approval
+        tx_hash = sp.bytes("test")
+        multi_sig_wallet.data.destination = recipient
+        multi_sig_wallet.data.amount = sp.mutez(25)
+        multi_sig_wallet.data.approvals = {}
+    
+        # approve transaction with one owner
+        scenario += multi_sig_wallet.approve(tx_hash).run(sender=owner1)
+    
+        # try to execute transaction with only one approval
+        scenario += multi_sig_wallet.approve(tx_hash).run(sender=owner2)
+        scenario.show(multi_sig_wallet.balance_of(owner1.address))
+        scenario.show(multi_sig_wallet.balance_of(owner2.address))
+        scenario.show(multi_sig_wallet.balance_of(owner3.address))
+    
+        # approve transaction with second owner
+        scenario += multi_sig_wallet.approve(tx_hash).run(sender=owner3)
+        scenario.show(multi_sig_wallet.balance_of(owner1.address))
+        scenario.show(multi_sig_wallet.balance_of(owner2.address))
+        scenario.show(multi_sig_wallet.balance_of(owner3.address))
+    
+        # reset approvals for all transactions
+        scenario += multi_sig_wallet.reset_approvals().run(sender=owner1)
+        scenario.show(multi_sig_wallet.data.approvals)
+
+    # run test scenario
+    test()
